2169b4d1113034cfcfaf9e9cb759e64b912ccc6c
felipe <carlosfelipemonte1997@hotmail.com>
2024-06-21 07:56:20 -0300

    training sqlite

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("Hello, world!");
 });
 
+app.post("/person", (request, response) => {
+  console.log(request.body);
+  response.json({
+    "statusCode": 200
+  });
+});
+
 app.listen(3000, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running`); 
 });
